5627064a04fb670ef75fa09840ef15055c0b1ce3
zheniaregbl <=>
2023-11-12 17:06:00 +0300

    Updated to 0.0.5 alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.0.2a"
+        versionName = "0.0.5a"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/com/syndicate/ptkscheduleapp/core/AppConstants.kt
+++ app/src/main/java/com/syndicate/ptkscheduleapp/core/AppConstants.kt
@@ -1,6 +1,6 @@
 package com.syndicate.ptkscheduleapp.core
 
 object AppConstants {
-    const val APP_VERSION = "0.0.2a"
+    const val APP_VERSION = "0.0.5a"
     const val BASE_URL = "http://109.172.80.105:5001/api/"
 }
