2854a8df71ba5c8f19ec40598eb80d867af4d5c8
zheniaregbl <=>
2023-12-19 16:48:03 +0300

    Updated to 1.0.2

--- app/src/main/java/com/syndicate/ptkscheduleapp/core/AppConstants.kt
+++ app/src/main/java/com/syndicate/ptkscheduleapp/core/AppConstants.kt
@@ -1,6 +1,6 @@
 package com.syndicate.ptkscheduleapp.core
 
 object AppConstants {
-    const val APP_VERSION = "1.0.1"
+    const val APP_VERSION = "1.0.2"
     const val BASE_URL = "http://109.172.80.105:5001/api/"
 }
